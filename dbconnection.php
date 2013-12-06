1f0a238b69aa10b34a67770d3042276d2c997e86
Derek Thurston <thegrit@gmail.com>
2013-12-06 11:04:39 -0500

    Added comment about required file modifications

--- dbconnection.php
+++ dbconnection.php
@@ -1,4 +1,8 @@
 <?php
+
+// Hey!  This file should be located at ../../dbconnection.php to work.  Please make sure you don't
+// leave it hanging out in public view.
+
 // Database connection constants
 define("DATABASE_HOST", "ENTER YOUR DB HOSTNAME");
 define("DATABASE_USERNAME", "ENTER YOUR USERNAME");
